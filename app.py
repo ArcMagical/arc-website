3081aa67df5905314457d6c5a3bb1fb7e8eaf4c3
ArcMagical <163700425+ArcMagical@users.noreply.github.com>
2024-03-25 17:51:24 +0000

    CSS update and render dynamic data

--- app.py
+++ app.py
@@ -1,10 +1,43 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+JOBS = [
+  {
+    'id': 1,
+    'title': 'Data Scientist',
+    'location': 'Moscow, Russia',
+    'salary': 'RUB 680,000'
+  },
+  {
+    'id': 2,
+    'title': 'Backend Engineer',
+    'location': 'Saint Petersburg, Russia',
+    'salary': 'RUB 250,000'
+  },
+  {
+    'id': 3,
+    'title': 'Frontend Engineer',
+    'location': 'Remote',
+    
+  },
+  {
+    'id': 4,
+    'title': 'Data Analyst',
+    'location': 'San Francisco, USA',
+    'salary': '$77000'
+  },
+]
+
 @app.route('/')
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html',
+                         jobs=JOBS,
+                         company_name='Arc')
+
+@app.route('/api/jobs')
+def list_jobs():
+  return jsonify()
   
 if __name__ == "__main__":
   app.run(host='0.0.0.0',debug=True)
